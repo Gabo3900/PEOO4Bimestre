9d8a7f24bd22ae550efe885719ca8739e9a1cf06
Gabriel Moura <gabo3900@outlook.com>
2019-10-21 13:42:22 -0300

    Init

---
+++ Lista21/Ex04/Jogador.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex04
+{
+    class Jogador : IComparable<Jogador>
+    {
+        private string nome;
+        private string email;
+        private int pontuacaoMaxima;
+        private DateTime data;
+        public Jogador(string n, string e)
+        {
+            nome = n;
+            email = e;
+        }
+        public void SetPontos(int p, DateTime d)
+        {
+            if (p >= 0) pontuacaoMaxima = p;
+            else throw new ArgumentOutOfRangeException();
+            if (d <= DateTime.Now) data = d;
+            else throw new ArgumentOutOfRangeException();
+        }
+        public override string ToString()
+        {
+            return $"{nome} {email} {pontuacaoMaxima} {data}";
+        }
+        public int CompareTo(Jogador j)
+        {
+            if (pontuacaoMaxima > j.pontuacaoMaxima) return 1;
+            if (pontuacaoMaxima < j.pontuacaoMaxima) return -1;
+
+            return data.CompareTo(j.data);
+        }
+    }
+}
---
+++ Lista21/Ex04/Jogo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex04
+{
+    class Jogo
+    {
+        private string nome;
+        private List<Jogador> jogs;
+        public Jogo(string n)
+        {
+            nome = n;
+        }
+        public void Inserir(Jogador j)
+        {
+            string email = j.ToString().Split()[1];
+            foreach (Jogador n in Listar())
+            {
+                if (email == n.ToString().Split()[1]) throw new EmailExistenteException();
+            }
+            jogs.Add(j);
+            jogs.Sort();
+        }
+        public List<Jogador> Listar()
+        {
+            return jogs;
+        }
+        public Jogador Top1()
+        {
+            return jogs[0];
+        }
+        public List<Jogador> Top10()
+        {
+            return jogs.GetRange(0, 10);
+        }
+        public override string ToString()
+        {
+            return $"{nome} {jogs.Count}";
+        }
+    }
+}
