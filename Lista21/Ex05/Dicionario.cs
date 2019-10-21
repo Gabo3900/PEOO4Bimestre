f80973696fd9eb5556f340a4b2333d79ab7c8a41
Gabriel Moura <gabo3900@outlook.com>
2019-10-21 13:49:27 -0300

    Init

---
+++ Lista21/Ex05/Dicionario.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex05
+{
+    class Dicionario<K, V>
+    {
+        private List<K> chaves = new List<K>();
+        private List<V> valores = new List<V>();
+        public List<K> Chaves
+        {
+            get { return chaves; }
+        }
+        public int Count
+        {
+            get { return chaves.Count; }
+        }
+        public V this[K i]
+        {
+            get { return valores[chaves.IndexOf(i)]; }
+            set { valores[chaves.IndexOf(i)] = value; }
+        }
+        public void Add(K chave, V valor)
+        {
+            if (chave == null) throw new ArgumentNullException();
+            if (Contains(chave)) throw new InvalidOperationException();
+            chaves.Add(chave);
+            valores.Add(valor);
+        }
+        public void Clear()
+        {
+            chaves.Clear();
+            valores.Clear();
+        }
+        public bool Contains(K chave)
+        {
+            return chaves.Contains(chave);
+        }
+        public bool Remove(K chave)
+        {
+            if (chaves.Contains(chave))
+            {
+                chaves.Remove(chave);
+                return true;
+            }
+            return false;
+        }
+    }
+}
