79f4775e23cb38f79aecaed9244deb03d42f767b
Gabriel Moura <gabo3900@outlook.com>
2019-10-21 13:32:37 -0300

    Init

---
+++ Lista21/Ex02/Retangulo.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex02
+{
+    class Retangulo
+    {
+        private double b;
+        private double h;
+        public Retangulo(double b, double h)
+        {
+            Base = b;
+            Altura = h;
+        }
+        public double Base
+        {
+            get
+            {
+                return b;
+            }
+            set
+            {
+                if (value >= 0) b = value;
+                else throw new ArgumentOutOfRangeException();
+            }
+        }
+        public double Altura
+        {
+            get
+            {
+                return h;
+            }
+            set
+            {
+                if (value >= 0) h = value;
+                else throw new ArgumentOutOfRangeException();
+            }
+        }
+        public double GetArea()
+        {
+            return Base * Altura;
+        }
+        public override string ToString()
+        {
+            return $"{Base} x {Altura}";
+        }
+    }
+}
