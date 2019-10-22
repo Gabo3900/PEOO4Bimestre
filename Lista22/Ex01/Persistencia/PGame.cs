0dfe41b2670eba39b10c40572be9bb5efed33e52
Gabriel Moura <gabo3900@outlook.com>
2019-10-22 09:35:20 -0300

    Init

--- Lista22/Ex01/Persistencia/PGame.cs
+++ Lista22/Ex01/Persistencia/PGame.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
+using Modelo;
 
 namespace Persistencia
 {
