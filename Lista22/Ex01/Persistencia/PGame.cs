3848dc3a24a272bd2ffa391b5c66834c71e1463d
Gabriel Moura <gabo3900@outlook.com>
2019-10-22 09:29:17 -0300

    Init

---
+++ Lista22/Ex01/Persistencia/PGame.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistencia
+{
+    public class PGame
+    {
+        private string arquivo = "Games.xml";
+        public List<Game> Open()
+        {
+            XmlSerializer x = new XmlSerializer(typeof(List<Game>));
+            StreamReader f = null;
+            List<Game> cs = null;
+            try
+            {
+                f = new StreamReader(arquivo, Encoding.Default);
+                cs = x.Deserialize(f) as List<Game>;
+            }
+            catch
+            {
+                cs = new List<Game>();
+            }
+            finally
+            {
+                if (f != null) f.Close();
+            }
+            return cs;
+        }
+        public void Save(List<Game> cs)
+        {
+            XmlSerializer x = new XmlSerializer(typeof(List<Game>));
+            StreamWriter f = new StreamWriter(arquivo, false, Encoding.Default);
+            x.Serialize(f, cs);
+            f.Close();
+        }
+    }
+}
