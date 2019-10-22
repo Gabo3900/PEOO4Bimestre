254d9eba6690aca4d98e0df7be53030225d0c038
Gabriel Moura <gabo3900@outlook.com>
2019-10-22 09:39:21 -0300

    Init

---
+++ Lista22/Ex01/Negocio/NGame.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Modelo;
+using Persistencia;
+
+namespace Negocio
+{
+    public class NGame
+    {
+        public List<Game> Select()
+        {
+            PGame p = new PGame();
+            return p.Open().OrderBy(c => c.Nome).ToList();
+        }
+        public void Insert(Game c)
+        {
+            PGame p = new PGame();
+            List<Game> cs = p.Open();
+            int m = 0;
+            foreach (Game x in cs) if (x.Id > m) m = x.Id;
+            c.Id = m + 1;
+            cs.Add(c);
+            p.Save(cs);
+        }
+        public void Update(Game c)
+        {
+            PGame p = new PGame();
+            List<Game> cs = p.Open();
+            for (int i = 0; i < cs.Count; i++)
+                if (cs[i].Id == c.Id)
+                {
+                    cs.RemoveAt(i);
+                    break;
+                }
+            cs.Add(c);
+            p.Save(cs);
+        }
+        public void Delete(Game c)
+        {
+            PGame p = new PGame();
+            List<Game> cs = p.Open();
+            for (int i = 0; i < cs.Count; i++)
+                if (cs[i].Id == c.Id)
+                {
+                    cs.RemoveAt(i);
+                    break;
+                }
+            p.Save(cs);
+        }
+    }
+}
