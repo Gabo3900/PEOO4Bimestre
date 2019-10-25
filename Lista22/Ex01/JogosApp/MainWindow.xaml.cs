26493296e8f1cb7e8cad9d8cfa7154ee10199993
Gabriel Moura <gabo3900@outlook.com>
2019-10-25 11:51:55 -0300

    Init

--- Lista22/Ex01/JogosApp/MainWindow.xaml.cs
+++ Lista22/Ex01/JogosApp/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Modelo;
+using Negocio;
 
 namespace JogosApp
 {
@@ -23,6 +25,44 @@ namespace JogosApp
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click_Insert(object sender, RoutedEventArgs e)
+        {
+            Game g = new Game();
+            NGame n = new NGame();
+            g.Id = 0;
+            g.Nome = txtNome.Text;
+            g.Fabricante = txtFab.Text;
+            g.DataCompra = DateTime.Parse(txtData.Text);
+            g.Estrelas = int.Parse(txtEstrela.Text);
+            n.Insert(g);
+            grid.ItemsSource = n.Select();
+        }
+
+        private void Button_Click_Delete(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_Update(object sender, RoutedEventArgs e)
+        {
+
+            Game c = new Game();
+            c.Id = int.Parse(txtId.Text);
+            g.Nome = txtNome.Text;
+            g.Fabricante = txtFab.Text;
+            g.DataCompra = DateTime.Parse(txtData.Text);
+            g.Estrelas = int.Parse(txtEstrela.Text);
+            NGame n = new NGame();
+            n.Update(c);
+            grid.ItemsSource = null;
+            grid.ItemsSource = n.Select();
+        }
+
+        private void Button_Click_Top10(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
